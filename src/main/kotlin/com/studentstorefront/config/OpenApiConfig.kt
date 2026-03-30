64798c6b742734fe400034d211471d5368f1badb
Chingis <chingis.saxonov@gmail.com>
2026-03-30 21:58:54 +0200

    Configure Swagger with JWT Auth

---
+++ src/main/kotlin/com/studentstorefront/config/OpenApiConfig.kt
@@ -0,0 +1,35 @@
+package com.studentstorefront.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Student Store Front API")
+                    .version("1.0")
+                    .description("API for Student Store Front application")
+            )
+            .addSecurityItem(SecurityRequirement().addList("Bearer Authentication"))
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "Bearer Authentication",
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
+            )
+    }
+}
