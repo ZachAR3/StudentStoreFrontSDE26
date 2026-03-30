ca17353081eedd0d52280ec1426eb5c409d45810
Chingis <chingis.saxonov@gmail.com>
2026-03-30 18:50:40 +0200

    Update repos

--- src/main/kotlin/com/studentstorefront/repository/PostRepository.kt
+++ src/main/kotlin/com/studentstorefront/repository/PostRepository.kt
@@ -5,13 +5,11 @@ import com.studentstorefront.entity.Post
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PostRepository: JpaRepository<Post, Long> {
-
     fun findByCategory(category: Category, pageable: Pageable): Page<Post>
-
     fun findBySellerSellerId(sellerId: Long, pageable: Pageable): Page<Post>
-
     fun findByIsSoldFalse(pageable: Pageable): Page<Post>
-
 }
--- src/main/kotlin/com/studentstorefront/repository/SellerRepository.kt
+++ src/main/kotlin/com/studentstorefront/repository/SellerRepository.kt
@@ -2,7 +2,9 @@ package com.studentstorefront.repository
 
 import com.studentstorefront.entity.Seller
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SellerRepository: JpaRepository<Seller, Long> {
     fun findByEmail(email: String): Seller?
     fun existsByEmail(email: String): Boolean
